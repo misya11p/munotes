c4e0d286e911b1fa1051ba340ef177ac01f79172
misya11p <miya33ps@gmail.com>
2022-11-11 14:25:43 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+setup(
+    name='munotes',
+    version="0.1.0",
+    description="",
+    long_description="",
+    # url='',
+    # author='',
+    # author_email='',
+    # license='',
+    # classifiers=[],
+    # keywords='',
+    install_requires=[],
+)
