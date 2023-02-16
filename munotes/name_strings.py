b15bae175201c4fd873f74b94a3a82aa6fcc1035
misya11p <miya33ps@gmail.com>
2023-02-16 14:58:56 +0900

    fix octave bug

--- munotes/name_strings.py
+++ munotes/name_strings.py
@@ -8,7 +8,7 @@ VALID_NOTE_PATTERN = r"[A-Ga-g][#♯+b♭-]?\d*"
 
 
 def note_name_formatting(
-    note_name_string: str,
+    note_name: str,
     octave: Optional[int]
 ) -> Tuple[str, int]:
     """
@@ -16,42 +16,43 @@ def note_name_formatting(
     'octave' argument is ignored if it is specified in the note_name_string.
 
     Args:
-        note_name_strings (str): string of note name
+        note_name (str): string of note name
         octave (Optional[int]): octave
 
     Returns:
         Tuple[str, int]: formatted note name and octave
     """
-    note_name = string_formatting(note_name_string)
-    assert re.match(NOTE_PATTERN, note_name), \
-        f"Input string {note_name} is invalid. Valid string: {VALID_NOTE_PATTERN}"
-    border = 2 if ('#' in note_name or 'b' in note_name) else 1
-    note_name = note_name[:border]
-    octave_str = note_name[border:]
+    form_note_name = string_formatting(note_name)
+    assert re.match(NOTE_PATTERN, form_note_name), \
+        f"'{note_name}' is invalid. Valid string: {VALID_NOTE_PATTERN}"
+    border = 2 if ('#' in form_note_name or 'b' in form_note_name) else 1
+    pitch_name = form_note_name[:border]
+    octave_str = form_note_name[border:]
     if not octave_str:
         assert octave, "Octave is not specified."
         assert isinstance(octave, int), \
             "Octave must be an integer. Input octave: {octave}"
     else:
         octave = int(octave_str)
-    return note_name, octave
+    return pitch_name, octave
 
 
-def chord_name_formatting(chord_name_string: str) -> Tuple[str, str]:
+def chord_name_formatting(chord_name: str) -> Tuple[str, str]:
     """
     Format chord name string and return it with chord type.
 
     Args:
-        chord_name_string (str): string of chord name
+        chord_name (str): string of chord name
 
     Returns:
         Tuple[str, str]: formatted chord name and chord type
     """
-    chord_name = string_formatting(chord_name_string)
-    note_search = re.match(NOTE_NAME_PATTERN, chord_name)
-    assert note_search, f"'{chord_name_string}' is an invalid string"
+    form_chord_name = string_formatting(chord_name)
+    note_search = re.match(NOTE_NAME_PATTERN, form_chord_name)
+    assert note_search, f"'{chord_name}' is an invalid string"
     border = note_search.end()
-    root_name, type = chord_name[:border], chord_name[border:]
+    root_name = form_chord_name[:border]
+    type = form_chord_name[border:]
     assert type in chord_names, f"'{type}' is an invalid chord type"
     return root_name, type
 
